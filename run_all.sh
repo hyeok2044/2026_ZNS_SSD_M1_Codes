68f2a0ae27f5922a4e7f2218c257fad394f84da4
hyeok2044 <hyeok2044@gmail.com>
2026-04-26 10:56:09 +0900

    hot fix on run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 ./kafka_storage_transition.sh ext4 /mnt/ext4/kafka-logs
-./run_experiment.sh ext4
+./run_experiment.sh ext4 /mnt/ext4/kafka-logs
 
 ./kafka_storage_transition.sh f2fs /mnt/f2fs/kafka-logs
-./run_experiment.sh f2fs
+./run_experiment.sh f2fs /mnt/f2fs/kafka-logs
