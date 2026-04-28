18999647114a35734b6c4c2d0063dec8402c5464
hyeok2044 <hyeok2044@gmail.com>
2026-04-28 17:06:40 +0900

    test4

--- producer.c
+++ producer.c
@@ -143,7 +143,7 @@ static rd_kafka_t *create_producer(const producer_options_t *options,
   rd_kafka_conf_set(
       conf, "max.request.size", "20971520", errstr, sizeof(errstr));
   rd_kafka_conf_set(conf, "linger.ms", "0", errstr, sizeof(errstr));
-  rd_kafka_conf_set(conf, "batch.size", "1200000", errstr, sizeof(errstr));
+  rd_kafka_conf_set(conf, "batch.size", "12000000", errstr, sizeof(errstr));
 
   set_config(conf, "acks", (char *)options->acks);
   rd_kafka_conf_set_dr_msg_cb(conf, dr_msg_cb);
